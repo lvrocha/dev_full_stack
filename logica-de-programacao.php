8b8f7697115bcb747d116572fb6fbbd18372425c
Lucas <lucasvilhenaenator@gmail.com>
2020-07-05 17:55:31 -0300

    refactor 2

--- logica-de-programacao.php
+++ logica-de-programacao.php
@@ -1,7 +1,7 @@
 <?php
-/** 
- * @Desc:  Pensando em todos os números naturais inferiores a 10 que são múltiplos 
- * de 3 ou 5, temos 3, 5, 6 e 9. Somando esses múltiplos obtemos o valor 23. Utilize 
+/**
+ * @Desc:  Pensando em todos os números naturais inferiores a 10 que são múltiplos
+ * de 3 ou 5, temos 3, 5, 6 e 9. Somando esses múltiplos obtemos o valor 23. Utilize
  * um algorítimo para calcular a soma de todos os múltiplos de 3 ou 5 abaixo de 1000
  */
 
@@ -23,27 +23,24 @@
 
 $vector = [];
 
-for ($i=1; $i < 1001; $i++) { 
+for ($i = 1; $i < 1001; $i++) {
     $vector[] += $i;
 }
 
-function div_3_and_5($value){
-    if (($value % 3) == 0) {
-        return $value;
-    }elseif (($value % 5) == 0){
+function div_3_and_5($value)
+{
+    if ((($value % 3) == 0) || (($value % 5) == 0)) {
         return $value;
     }
 }
 
-function sum($sum, $item){
+function sum($sum, $item)
+{
     $sum += $item;
     return $sum;
 }
 
-$filtered = array_filter($vector,"div_3_and_5");
+$filtered = array_filter($vector, "div_3_and_5");
 $sum = array_reduce($filtered, "sum");
 
 echo $sum;
-
-
-
