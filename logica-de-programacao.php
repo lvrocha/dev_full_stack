62aca1744272497ad8ef99611d2d929f1d9b2265
Lucas <lucasvilhenaenator@gmail.com>
2020-07-05 17:53:34 -0300

    refactor

--- logica-de-programacao.php
+++ logica-de-programacao.php
@@ -5,18 +5,45 @@
  * um algorítimo para calcular a soma de todos os múltiplos de 3 ou 5 abaixo de 1000
  */
 
+// $vector = [];
+// for ($i = 1; $i < 1001; $i++) {
+//     if (($i % 3) == 0) {
+//         $vector[] += $i;
+//     }elseif (($i % 5) == 0){
+//         $vector[] += $i;
+//     }
+// }
+
+// $sum = 0;
+// foreach ($vector as $item) {
+//     $sum += $item;
+// }
+
+// echo "<p>{$sum}</p>";
+
 $vector = [];
-for ($i = 1; $i < 1001; $i++) {
-    if (($i % 3) == 0) {
-        $vector[] += $i;
-    }elseif (($i % 5) == 0){
-        $vector[] += $i;
+
+for ($i=1; $i < 1001; $i++) { 
+    $vector[] += $i;
+}
+
+function div_3_and_5($value){
+    if (($value % 3) == 0) {
+        return $value;
+    }elseif (($value % 5) == 0){
+        return $value;
     }
 }
 
-$sum = 0;
-foreach ($vector as $item) {
+function sum($sum, $item){
     $sum += $item;
+    return $sum;
 }
 
+$filtered = array_filter($vector,"div_3_and_5");
+$sum = array_reduce($filtered, "sum");
+
 echo $sum;
+
+
+
